53a0b9053950168f341648e6228623fe47011301
Theodore Turocy <ted.turocy@gmail.com>
2020-07-22 16:13:09 +0100

    Style retailer task

--- jmk-new_project/GiftCard/models.py
+++ jmk-new_project/GiftCard/models.py
@@ -14,7 +14,20 @@ class Subsession(BaseSubsession):
 class Group(BaseGroup):
     pass
 class Player(BasePlayer):
-    GiftCard1 = models.StringField(choices=[['1', 'A'], ['2', 'B'], ['3', 'C'], ['4', 'D'], ['5', 'E']], label="Which retailers' gift card would you like?")
-    GiftCard2 = models.StringField(choices=[['1', 'A - Komodo'], ['2', 'B - ASOS'], ['3', 'C - Next'], ['4', 'D - H&M'], ['5', 'E - Boohoo']], label="Which retailers' gift card would you like?")
-    RankingAgreement = models.BooleanField(choices=[[True, 'Yes'], [False, 'No']], label='Do you agree with the ethical rankings of the retailers?')
+    GiftCard1 = models.StringField(
+        choices=[['1', 'A'], ['2', 'B'], ['3', 'C'], ['4', 'D'], ['5', 'E']],
+        label="Which retailer's gift card would you like?",
+        widget=widgets.RadioSelectHorizontal
+    )
+    GiftCard2 = models.StringField(
+        choices=[['1', 'A - Komodo'], ['2', 'B - ASOS'], ['3', 'C - Next'],
+                 ['4', 'D - H&M'], ['5', 'E - Boohoo']],
+        label="Which retailers' gift card would you like?",
+        widget=widgets.RadioSelectHorizontal
+    )
+    RankingAgreement = models.BooleanField(
+        choices=[[True, 'Yes'], [False, 'No']],
+        label='Do you agree with the ethical rankings of the retailers?',
+        widget=widgets.RadioSelectHorizontal
+    )
     RAWhy = models.LongStringField(blank=True, label='If not, why not?')
